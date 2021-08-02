04891d58af9cddac962ab0be293e26f7e6d14686
Nikolas Matias <nikolas.matias500@gmail.com>
2021-08-02 06:48:56 -0300

    Changes in styles.tsx

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,1 +1,26 @@
 import styled from 'styled-components';
+
+const primaryColors = {
+    LIGHT_RED: 'hsl(356, 100%, 66%)',
+    VERY_LIGHT_RED: 'hsl(355, 100%, 74%)',
+    VERY_DARK_BLUE: 'hsl(208, 49%, 24%)',
+}
+
+const neutralColors = {
+    WHITE: 'hsl(0, 0%, 100%)',
+    GRAYISH_BLUE: 'hsl(240, 2%, 79%)',
+    VERY_DARK_GRAYISH_BLUE: 'hsl(207, 13%, 34%)',
+    VERY_DARK_BLACK_BLUE: 'hsl(240, 10%, 16%)'
+}
+
+const gradientColors = {
+    VERY_LIGHT_RED: 'hsl(13, 100%, 72%)',
+    LIGHT_RED: 'hsl(353, 100%, 62%)',
+    VERY_DARK_GRAY_BLUE: 'hsl(237, 17%, 21%)',
+    VERY_DARK_DESATURATED_BLUE: 'hsl(237, 23%, 32%)'
+}
+
+export const Title = styled.h1`
+    
+`
+
